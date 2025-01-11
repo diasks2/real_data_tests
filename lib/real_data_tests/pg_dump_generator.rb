1e6d017098dfd9a8c427141f79321b4a9fed6a54
Kevin Dias <diasks2@gmail.com>
2025-01-11 09:46:59 +0900

    Handle UUIDs

--- lib/real_data_tests/pg_dump_generator.rb
+++ lib/real_data_tests/pg_dump_generator.rb
@@ -8,18 +8,31 @@ module RealDataTests
       tables_with_records = @records.group_by { |record| record.class.table_name }
 
       tables_with_records.map do |table_name, records|
-        ids = records.map(&:id).join(',')
+        ids = records.map { |r| quote_value(r.id) }.join(',')
 
         "pg_dump #{connection_options} --table=#{table_name} " \
         "--data-only --column-inserts " \
-        "--where='id IN (#{ids})'"
+        "--where \"id IN (#{ids})\""
       end.join(" && ")
     end
 
     private
 
+    def quote_value(value)
+      if value.is_a?(String) # Handle UUIDs
+        "'#{value}'"
+      else
+        value
+      end
+    end
+
     def connection_options
-      config = ActiveRecord::Base.connection_config
+      config = if ActiveRecord::Base.respond_to?(:connection_db_config)
+        ActiveRecord::Base.connection_db_config.configuration_hash
+      else
+        ActiveRecord::Base.connection_config
+      end
+
       options = []
       options << "-h #{config[:host]}" if config[:host]
       options << "-p #{config[:port]}" if config[:port]
