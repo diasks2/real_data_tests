4245fb96bf3ba4ffeaaaa6a3552d08bb46849a22
Kevin Dias <diasks2@gmail.com>
2025-01-11 11:32:02 +0900

    Update anonymizer

--- lib/real_data_tests/data_anonymizer.rb
+++ lib/real_data_tests/data_anonymizer.rb
@@ -1,40 +1,52 @@
+# lib/real_data_tests/data_anonymizer.rb
+require 'faker'
+
 module RealDataTests
   class DataAnonymizer
-    def initialize(records)
-      @records = records
-      @anonymization_rules = RealDataTests.configuration.anonymization_rules
+    def initialize(configuration)
+      @configuration = configuration
     end
 
-    def anonymize_records
-      @records.each do |record|
-        anonymize_record(record) if should_anonymize?(record)
+    def anonymize_record(record)
+      return record unless should_anonymize?(record)
+
+      anonymization_rules = @configuration.anonymization_rules[record.class.name]
+
+      anonymization_rules.each do |attribute, anonymizer|
+        begin
+          new_value = case anonymizer
+                     when String
+                       # Handle legacy string-based Faker calls
+                       process_faker_string(anonymizer)
+                     when Proc, Lambda
+                       # Handle lambda-based anonymizers
+                       anonymizer.call(record)
+                     else
+                       raise Error, "Unsupported anonymizer type: #{anonymizer.class}"
+                     end
+
+          record.send("#{attribute}=", new_value)
+        rescue => e
+          raise Error, "Failed to anonymize #{attribute} using #{anonymizer.inspect}: #{e.message}"
+        end
       end
+
+      record
     end
 
     private
 
     def should_anonymize?(record)
-      @anonymization_rules.key?(record.class.name)
+      @configuration.anonymization_rules.key?(record.class.name)
     end
 
-    def anonymize_record(record)
-      rules = @anonymization_rules[record.class.name]
-
-      rules.each do |attribute, faker_method|
-        begin
-          # Parse the faker method string (e.g., "Faker::Name.first_name")
-          faker_class, faker_method = faker_method.split('::')[1..].join('::').split('.')
-
-          # Get the Faker class dynamically
-          faker_class = Object.const_get("Faker::#{faker_class}")
-
-          # Set the anonymized value
-          record.send("#{attribute}=", faker_class.send(faker_method))
-          record.save!
-        rescue => e
-          raise Error, "Failed to anonymize #{attribute} using #{faker_method}: #{e.message}"
-        end
-      end
+    def process_faker_string(faker_method)
+      # Support legacy string format like "Faker::Name.first_name"
+      faker_class, faker_method = faker_method.split('::')[1..].join('::').split('.')
+      faker_class = Object.const_get("Faker::#{faker_class}")
+      faker_class.send(faker_method)
+    rescue => e
+      raise Error, "Failed to process Faker method '#{faker_method}': #{e.message}"
     end
   end
 end
