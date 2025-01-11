e3ed437c38759ac8227a12923290518079d696c9
Kevin Dias <diasks2@gmail.com>
2025-01-11 09:52:52 +0900

    Update pg dump command

--- lib/real_data_tests/pg_dump_generator.rb
+++ lib/real_data_tests/pg_dump_generator.rb
@@ -7,13 +7,24 @@ module RealDataTests
     def generate
       tables_with_records = @records.group_by { |record| record.class.table_name }
 
-      tables_with_records.map do |table_name, records|
+      commands = tables_with_records.map do |table_name, records|
         ids = records.map { |r| quote_value(r.id) }.join(',')
 
-        "pg_dump #{connection_options} --table=#{table_name} " \
-        "--data-only --column-inserts " \
-        "--where \"id IN (#{ids})\""
-      end.join(" && ")
+        # Create a temporary view for the selected records
+        tmp_view_name = "tmp_#{table_name}_#{Time.now.to_i}"
+        view_creation = "psql #{connection_options} -c \"CREATE VIEW #{tmp_view_name} AS SELECT * FROM #{table_name} WHERE id IN (#{ids})\""
+
+        # Dump the view
+        dump_command = "pg_dump #{connection_options} --table=#{tmp_view_name} --data-only --column-inserts"
+
+        # Clean up the view
+        cleanup_command = "psql #{connection_options} -c \"DROP VIEW #{tmp_view_name}\""
+
+        # Combine commands and fix the table name in the output
+        "(#{view_creation} && #{dump_command} | sed 's/#{tmp_view_name}/#{table_name}/g' && #{cleanup_command})"
+      end
+
+      commands.join(" && ")
     end
 
     private
